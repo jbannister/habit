7a11593114f503bc8edacc6ab25e595baec5a925
Jan Bannister <jan.bannister@gmail.com>
2022-11-16 22:50:33 +0000

    list_habit

--- habit.py
+++ habit.py
@@ -2,16 +2,31 @@ import argparse
 import pandas as pd
 from os.path import exists
 
+def create_habit() -> pd.DataFrame:
+    if not exists('habit.csv'):
+        with open('habit.csv', 'w') as f:
+            habit_dist = {"date":[], "habit":[], "yes_no":[]}
+            # ToDo: Add columns for habit, date, and yes/no
+            return pd.DataFrame.from_dict(habit_dist) 
+    else:
+        return pd.read_csv('habit.csv')
+
+def list_habit(habit_df: pd.DataFrame) -> pd.DataFrame:
+    print(habit_df.head())
+
+df_habit = create_habit()
+
 def habit():
+    global df_habit
     parser = argparse.ArgumentParser(
                     prog = 'Habit',
                     description = 'What the program does',
                     epilog = 'Text at the bottom of help')
 
-    parser.add_argument("-c", "--create") # C - Create a new habit entry
-    parser.add_argument("-l", "--list")   # R - List all habit entries
-    parser.add_argument("-u", "--update") # U - Update a habit entry
-    parser.add_argument("-d", "--delete") # D - Delete a habit entry
+    parser.add_argument("-c", "--create")               # C - Create a new habit entry
+    parser.add_argument("list")                         # R - List all habit entries
+    parser.add_argument("-u", "--update")               # U - Update a habit entry
+    parser.add_argument("-d", "--delete")               # D - Delete a habit entry
 
     #parser.add_argument('filename')           # positional argument
     #parser.add_argument('-c', '--count')      # option that takes a value
@@ -25,17 +40,10 @@ def habit():
 
     if args.create:
         df_habit = create_habit()
-        print(df_habit.head())
-        #print(df_habit)
 
-def create_habit() -> pd.DataFrame:
-    if not exists('habit.csv'):
-        with open('habit.csv', 'w') as f:
-            habit_dist = {"date":[], "habit":[], "yes_no":[]}
-            # ToDo: Add columns for habit, date, and yes/no
-            return pd.DataFrame.from_dict(habit_dist) 
-    else:
-        return pd.read_csv('habit.csv')
+    if args.list:
+        list_habit(df_habit)
+
 
 if __name__ == "__main__":
     habit()
